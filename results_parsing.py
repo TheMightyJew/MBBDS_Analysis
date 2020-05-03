ae66beb68e3417011e07ba7dd77380d3cab27826
TheMightyJew <38401349+TheMightyJew@users.noreply.github.com>
2020-05-03 19:19:10 +0300

    fixed mean calculations

--- results_parsing.py
+++ results_parsing.py
@@ -2,37 +2,37 @@ import pandas as pd
 
 file = open("results_28-04-2020_17-26-35.txt", "r")
 cols = ['Number Of Pancakes', 'Gap', 'Problem ID', 'Start state', 'Goal state', 'Initial Heuristic'
-                            , 'Algorithm', 'Memory', 'Status', 'States Expanded', 'Runtime(seconds)']
+    , 'Algorithm', 'Memory', 'Status', 'States Expanded', 'Runtime(seconds)']
 resultsDF = pd.DataFrame()
 resDict = dict.fromkeys(cols)
 gapStr = 'GAP-0'
 resDict['Problem ID'] = 0
 for line in file:
     line = line.replace('\t', '').replace('\n', '')
-    splittedLine = line.replace(gapStr,' ' + gapStr + ' ').split()
+    splittedLine = line.replace(gapStr, ' ' + gapStr + ' ').split()
     if 'TestPancakeHard' in line:
-        resDict['Number Of Pancakes'] = int(splittedLine[splittedLine.index('TestPancakeHard:(Pancakes:')+1].replace(',', ''))
-        resDict['Gap'] = int(splittedLine[splittedLine.index('Gap:')+1].replace(')', ''))
+        resDict['Number Of Pancakes'] = int(
+            splittedLine[splittedLine.index('TestPancakeHard:(Pancakes:') + 1].replace(',', ''))
+        resDict['Gap'] = int(splittedLine[splittedLine.index('Gap:') + 1].replace(')', ''))
         gapStr = 'GAP-' + str(resDict['Gap'])
     elif 'Problem' in line:
-        problemID = int(splittedLine[splittedLine.index('Problem')+1])
-        if problemID > resDict['Problem ID']:
-            resDict['Problem ID'] = problemID
+        problemID = int(splittedLine[splittedLine.index('Problem') + 1])
+        resDict['Problem ID'] = problemID
         line = next(file).replace('\t', '').replace('\n', '')
-        resDict['Start state'] = line[line.index(':')+1:-1]
+        resDict['Start state'] = line[line.index(':') + 1:-1]
         line = next(file).replace('\t', '').replace('\n', '')
-        resDict['Goal state'] = line[line.index(':')+1:-1]
+        resDict['Goal state'] = line[line.index(':') + 1:-1]
         line = next(file).replace('\t', '').replace('\n', '')
-        resDict['Initial Heuristic'] = float(line[line.index('heuristic')+len('heuristic')+1:])
+        resDict['Initial Heuristic'] = float(line[line.index('heuristic') + len('heuristic') + 1:])
     elif gapStr in line:
-        resDict['Algorithm'] = splittedLine[splittedLine.index(gapStr)+1]
+        resDict['Algorithm'] = splittedLine[splittedLine.index(gapStr) + 1]
         if 'memory' in line:
             if 'MBBDS' in resDict['Algorithm']:
                 memoryStr = 'Memory_percentage_from_MM='
-                resDict['Memory'] = int(splittedLine[splittedLine.index('memory')+2])
+                resDict['Memory'] = int(splittedLine[splittedLine.index('memory') + 2])
                 resDict['Algorithm'] += '(' + line[line.index(memoryStr) + len(memoryStr):][:4] + ')'
             else:
-                resDict['Memory'] = float(splittedLine[splittedLine.index('using')+1])
+                resDict['Memory'] = float(splittedLine[splittedLine.index('using') + 1])
         else:
             resDict['Memory'] = '-'
         if 'fail' in line:
@@ -47,32 +47,34 @@ for line in file:
             elif 'elapsed;' in splittedLine:
                 resDict['Runtime(seconds)'] = float(splittedLine[splittedLine.index('elapsed;') - 1].replace('s', ''))
         resultsDF = resultsDF.append(resDict, ignore_index=True)
+resultsDF = resultsDF[cols]
+resultsDF.to_csv('results.csv')
 
-resultsDF[cols].to_csv('results.csv')
-
-analysisCols = ['Gap', 'Algorithm', 'Failed', 'Mean Expansions', 'Mean Runtime']
+analysisCols = ['Gap', 'Algorithm', 'Failed(Out Of 100)', 'Mean Expansions', 'Mean Runtime(Seconds)']
 analysisDF = pd.DataFrame()
 analysisDict = dict.fromkeys(analysisCols)
 import numpy as np
-MBBDSFailSum = False
-for gap in resultsDF['Gap'].unique():
-        for algo in resultsDF['Algorithm'].unique():
-            analysisDict['Gap'] = gap
-            analysisDict['Algorithm'] = algo
-            gapRows = (resultsDF['Gap'] == gap)
-            algoRows = (resultsDF['Algorithm'] == algo)
-            finishedRows = (resultsDF['Status'] == 1)
-            if MBBDSFailSum and 'MBBDS' in algo:
-                analysisDict['Failed'] += len(resultsDF[gapRows & algoRows & (resultsDF['Status'] == 0)])
-            else:
-                analysisDict['Failed'] = len(resultsDF[gapRows & algoRows & (resultsDF['Status'] == 0)])
-            analysisDict['Mean Expansions'] = np.mean(resultsDF[gapRows & algoRows & finishedRows]['States Expanded'])
-            analysisDict['Mean Runtime'] = np.mean(resultsDF[gapRows & algoRows & finishedRows]['Runtime(seconds)'])
-            analysisDF = analysisDF.append(analysisDict, ignore_index=True)
-            if 'MBBDS' in algo:
-                MBBDSFailSum = True
-            else:
-                MBBDSFailSum = False
 
+MBBDSFailSum = False
+
+failedRows = (resultsDF['Status'] == 0)
+for gap in resultsDF['Gap'].unique():
+    analysisDict['Gap'] = gap
+    gapRows = (resultsDF['Gap'] == gap)
+    finished_problems_Rows = (resultsDF['Problem ID'].isin(set(resultsDF[gapRows]['Problem ID'])-set(resultsDF[gapRows & (failedRows)]['Problem ID'])))
+    for algo in resultsDF['Algorithm'].unique():
+        analysisDict['Algorithm'] = algo
+        algoRows = (resultsDF['Algorithm'] == algo)
+        if MBBDSFailSum and 'MBBDS' in algo:
+            analysisDict['Failed(Out Of 100)'] += len(resultsDF[gapRows & algoRows & failedRows])
+        else:
+            analysisDict['Failed(Out Of 100)'] = len(resultsDF[gapRows & algoRows & failedRows])
+        analysisDict['Mean Expansions'] = int(np.mean(resultsDF[gapRows & algoRows & finished_problems_Rows]['States Expanded']))
+        analysisDict['Mean Runtime(Seconds)'] = round(np.mean(resultsDF[gapRows & algoRows & finished_problems_Rows]['Runtime(seconds)']) ,3)
+        analysisDF = analysisDF.append(analysisDict, ignore_index=True)
+        if 'MBBDS' in algo:
+            MBBDSFailSum = True
+        else:
+            MBBDSFailSum = False
 
 analysisDF[analysisCols].to_csv('analysis.csv')
